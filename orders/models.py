ef9c1667bf0db34b0adef0aa1df4d3e8df4cbda0
Александр Михайлов <38732070+alexmind35@users.noreply.github.com>
2020-01-22 16:50:34 +0300

    Модели перед makemigrations

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField("Название", max_length=250)
+    price = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Услуги"
+        verbose_name_plural = "Услуги"
+
+
+class Order(models.Model):
+    amount = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    services = models.ForeignKey(Service, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = "Заказы"
+        verbose_name_plural = "Заказы"
+
+
+class UserCar(models.Model):
+    phone = models.IntegerField()
+    photo = models.ImageField("Фотография", upload_to="orders/users", default="", blank=True)
+    orders = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+
