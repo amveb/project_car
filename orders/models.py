8d4b8109a7b3cc7b1658a0909abde7ab87b4b7ba
Александр Михайлов <38732070+alexmind35@users.noreply.github.com>
2020-01-23 15:23:14 +0300

    Модели перед makemigrations, версия 4.0

--- orders/models.py
+++ orders/models.py
@@ -27,7 +27,7 @@ class Account(models.Model):
     photo = models.ImageField("Фотография", upload_to="orders/users", default="", blank=True)
     car = models.CharField("Автомобиль", max_length=250)
     testimonial = models.TextField("Отзыв")
-    orders = models.OneToOneField(Order, on_delete=models.CASCADE, null=True)
+    orders = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name = "Личный кабинет"
