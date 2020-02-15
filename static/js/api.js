020ad18fe56a7af912501d28ee6d5b937917c29a
Александр Михайлов <38732070+alexmind35@users.noreply.github.com>
2020-02-15 11:02:06 +0300

    Оформить заказ - выборка услуг пользователем

--- static/js/api.js
+++ static/js/api.js
@@ -1,18 +1,48 @@
 new Vue({
-        el: '#services_order',
+        el: '#vueOrder',
         data: {
-            message: 'Привет, Vue!!!!!',
-            services: []
+            services: [],
+            selectedOrder: []
         },
+    computed: {
+              itog() {
+                let result = 0;
+                for (
+                  var i = 0, _len = this.selectedOrder.length;
+                  i < _len;
+                  i++
+                ) {
+                  result += this.selectedOrder[i].price;
+                }
+                return result;
+              }
+            },
+            methods: {
+              changeOrder(service) {
+                const findService = this.selectedOrder.find(
+                  x => x.name === service.name
+                );
+                if (findService) {
+                  this.selectedOrder.splice(
+                    this.selectedOrder.indexOf(findService),
+                    1
+                  );
+                } else {
+                  this.selectedOrder.push(service);
+                }
+                console.log(this.selectedOrder);
+              }
+            },
         created: function () {
             const vm = this;
             axios.get('http://127.0.0.1:8000/api/services/?format=json')
                 .then(function (response) {
                     vm.services = response.data
                     console.log(response.data)
-                    console.log('Успешноооооо')
+                    console.log('Успешно')
 
                 })
         }
     }
 )
+
