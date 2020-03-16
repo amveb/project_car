fdba327d054c946a59682ae9feb0fbf36a81d4c2
Александр Михайлов <38732070+alexmind35@users.noreply.github.com>
2020-03-16 22:11:25 +0300

    Настройка шаблонов

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,58 @@
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from orders.models import Service
 
-def services_page(request):
-    return render(request, "pages/service_page.html", {'services': Service.objects.all()})
+
+def index_page(request):
+    return render(request, "landing_page.html", {'services': Service.objects.all()})
 
 
-def account_page(request):
-    return render(request, "pages/account_page.html")
+
+# def dashboard_services_page(request):
+#     return render(request, "dashboard/dashboard_page.html")
+
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super(OwnerMixin, self).get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(OwnerEditMixin, self).form_valid(form)
+
+
+class OwnerServiceMixin(OwnerMixin, LoginRequiredMixin):
+    model = Service
+    fields = ['name', 'price']
+    success_url = reverse_lazy('manage_service_list')
+
+
+class OwnerServiceEditMixin(OwnerServiceMixin, OwnerEditMixin):
+    fields = ['name', 'price']
+    success_url = reverse_lazy('manage_service_list')
+    template_name = 'dashboard/services/services_add.html'
+
+class ManageServiceListView(OwnerServiceMixin, ListView):
+    template_name = 'dashboard/services/services_list.html'
+
+
+class ServiceCreateView(PermissionRequiredMixin, OwnerServiceEditMixin, CreateView):
+    permission_required = 'services.add_course'
+
+
+class ServiceUpdateView(PermissionRequiredMixin, OwnerServiceEditMixin, UpdateView):
+    permission_required = 'services.change_course'
+
+
+class ServiceDeleteView(PermissionRequiredMixin, OwnerServiceMixin, DeleteView):
+    template_name = 'dashboard/services/services_list.html'
+    success_url = reverse_lazy('manage_service_list')
+    permission_required = 'services.delete_service'
